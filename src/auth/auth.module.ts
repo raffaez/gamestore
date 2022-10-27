d2eb7af2c525f2ff3239e5ff45690d2aa7cb53c3
raffaez <rafaeleasouzas@gmail.com>
2022-10-26 23:00:59 -0300

    update: imports UserModule, PassportModule and JwtModule; adds providers: AuthService, LocalStrategy and JwtStrateg; adds controller: AuthController

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,24 @@
 import { Module } from "@nestjs/common";
-import { Bcrypt } from "./bcrypt/bcrypt";
+import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
+import { UserModule } from "../user/user.module";
+import { jwtConstants } from "./constants/constants";
+import { AuthController } from "./controllers/auth.controller";
+import { AuthService } from "./services/auth.service";
+import { JwtStrategy } from "./strategies/jwt.strategy";
+import { LocalStrategy } from "./strategies/local.strategy";
 
 @Module({
-  imports: [],
-  providers: [Bcrypt],
-  controllers: [],
-  exports: [Bcrypt]
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '24h' }
+    })
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  controllers: [AuthController],
+  exports: []
 })
 export class AuthModule {}
