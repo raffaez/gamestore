683a893103f7b153c7c5b901b37d7c1c797f6e16
raffaez <rafaeleasouzas@gmail.com>
2022-10-26 22:56:11 -0300

    feat: implements strategies (local and jwt)

---
+++ src/auth/strategies/local.strategy.ts
@@ -0,0 +1,22 @@
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { Strategy } from "passport-local";
+import { MessagesHelper } from "../../helpers/messages.helper";
+import { AuthService } from "../services/auth.service";
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy){
+    constructor(private authService: AuthService){
+        super({
+            usernameField: 'email'
+        });
+    }
+
+    async validate(email: string, password: string): Promise<any>{
+        const user = await this.authService.validateUser(email, password);
+
+        if(!user) throw new UnauthorizedException(MessagesHelper.PASSWORD_OR_EMAIL_INVALID);
+
+        return user;
+    }
+}
